4cc7f2c037e8e95e44949a76440782fc50793076
Shun Huang <zsh@shunsvineyard.info>
2021-09-10 00:24:59 -0700

    Added unit tests.

---
+++ tests/test_table_store.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+
+from datainventory import _internal_store
+from datainventory import common
+from datainventory import table_store
+
+
+def test_simple_case():
+    engine = sqlalchemy.create_engine(
+        "sqlite+pysqlite:///:memory:", echo=True, future=True
+    )
+    metadata = sqlalchemy.MetaData(bind=engine)
+
+    store = table_store.TableStore(
+        create_key=_internal_store.CREATE_KEY,
+        metadata=metadata,
+        connection=engine.connect(),
+    )
+
+    schema = {"scale": common.ColumnType.String, "value": common.ColumnType.Float}
+    store.create_table(table_name="temperature", columns=schema)
+
+    data = [{"scale": "F", "value": 97.9}, {"scale": "C", "value": 23.7}]
+    store.insert(table_name="temperature", values=data)
+
